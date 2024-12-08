3d5f4a6830bb32ee04ea580cab038646f4c3af46
barry <highgogo@pm.me>
2024-12-08 15:52:32 +0800

    update dep

--- app/src/main/java/com/yyyit/myapplication/ui/theme/Theme.kt
+++ app/src/main/java/com/yyyit/myapplication/ui/theme/Theme.kt
@@ -25,16 +25,6 @@ private val LightColorScheme = lightColorScheme(
         primary = Purple40,
         secondary = PurpleGrey40,
         tertiary = Pink40
-
-        /* Other default colors to override
-    background = Color(0xFFFFFBFE),
-    surface = Color(0xFFFFFBFE),
-    onPrimary = Color.White,
-    onSecondary = Color.White,
-    onTertiary = Color.White,
-    onBackground = Color(0xFF1C1B1F),
-    onSurface = Color(0xFF1C1B1F),
-    */
 )
 
 @Composable
@@ -45,7 +35,7 @@ fun MyApplicationTheme(
         content: @Composable () -> Unit
 ) {
     val colorScheme = when {
-        dynamicColor && Build.VERSION.SDK_INT >= Build.VERSION_CODES.S -> {
+        dynamicColor  -> {
             val context = LocalContext.current
             if (darkTheme) dynamicDarkColorScheme(context) else dynamicLightColorScheme(context)
         }
